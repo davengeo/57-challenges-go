03a790711ed07ea4cf8760d9478445e2c47e6726
Navarro Alvarez, D <d.navarro.alvarez@accenture.com>
2015-08-31 17:30:01 +0300

    solved
    Module [57-challenges-go]:
      + mad-lib.go

---
+++ 4.mad-lib/mad-lib.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"bufio"
+	"os"
+)
+
+type Part struct{
+	name string
+	content string
+}
+
+func read_line() string {
+	reader := bufio.NewReader(os.Stdin)
+	input, _, _ := reader.ReadLine()
+	return string(input)
+}
+
+func main() {
+
+	parts := [...]string {
+		"noun",
+		"verb",
+		"adjective",
+		"adverb",
+	}
+
+	var content [4]string
+
+	for i, v := range parts {
+		fmt.Println("Give me a " + v + "?")
+		content[i] = read_line()
+	}
+
+	fmt.Printf("So... do you %s your %s %s %s? That's hilarious!", content[1], content[2], content[0], content[3])
+}
