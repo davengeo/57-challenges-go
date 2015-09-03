7aee592131e614e6bdb1eb69c1a57fce1593ae50
Navarro Alvarez, D <d.navarro.alvarez@accenture.com>
2015-09-03 15:12:41 +0300

    solved
    Module [57-challenges-go]:
      * area-rectangle.go

--- 7.area-rectangle/area-rectangle.go
+++ 7.area-rectangle/area-rectangle.go
@@ -5,8 +5,11 @@ import (
 	"fmt"
 )
 func main() {
+	const factor float64 = 0.09290304
 	length:=shared.Get_number_with_question("What is the length of the room in feet?")
 	width:=shared.Get_number_with_question("What is the width of the room in feet?")
-	fmt.Printf("%d %d \n", length, width)
+	fmt.Printf("You entered dimensions of %d feet by %d feet.\n", length, width)
+	fmt.Printf("The area is \n%d square feet. \n%f square meters.\n", length*width, float64(length*width)*factor)
+	fmt.Printf("=========================")
 }
 
