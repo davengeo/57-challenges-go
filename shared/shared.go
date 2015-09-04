f869d02866558c8c9aaddace8931648d7298559d
Navarro Alvarez, D <d.navarro.alvarez@accenture.com>
2015-09-04 15:00:18 +0300

    add read float64, need some refactoring maybe
    Module [57-challenges-go]:
      * self-checkout.go

--- shared/shared.go
+++ shared/shared.go
@@ -25,3 +25,22 @@ func Get_number_with_question(pos string) int {
 	}
 	return number
 }
+
+func read_float() (float64, error) {
+	reader := bufio.NewReader(os.Stdin)
+	input, _, _ := reader.ReadLine()
+	return strconv.ParseFloat(string(input), 10)
+}
+
+func Get_float_with_question(pos string) float64 {
+	var (
+		number float64
+		err error
+	)
+	fmt.Println(pos)
+	number,err=read_float()
+	if (err!=nil) {
+		Get_float_with_question(pos)
+	}
+	return number
+}
